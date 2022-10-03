22f24432c622dc0e70879f60773c6c850f2e751d
Rishabh Kumar <rishabhkaushal1796@gmail.com>
2022-10-04 03:06:11 +0530

    Alan working with react app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import alanBtn from '@alan-ai/alan-sdk-web'
+
+const alanKey =
+  'e9aa8d57d641bcca2d97eb57884f4f782e956eca572e1d8b807a3e2338fdd0dc/stage'
 
 const App = () => {
+  useEffect(() => {
+    alanBtn({
+      key: alanKey,
+      onCommand: ({ command }) => {
+        if (command === 'testCommand') {
+          alert('This code was executed')
+        }
+      },
+    })
+  }, [])
   return <div>hello AI</div>
 }
 
