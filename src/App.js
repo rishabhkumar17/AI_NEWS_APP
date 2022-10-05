b6d80dda0d6a784064048c8d791e68e55c8ccb8d
Rishabh Kumar <rishabhkaushal1796@gmail.com>
2022-10-05 20:26:41 +0530

    useState

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import React, { useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import alanBtn from '@alan-ai/alan-sdk-web'
 
 const alanKey =
   'e9aa8d57d641bcca2d97eb57884f4f782e956eca572e1d8b807a3e2338fdd0dc/stage'
 
 const App = () => {
+  const [newsArticles, setNewsArticles] = useState([])
+
   useEffect(() => {
     alanBtn({
       key: alanKey,
       onCommand: ({ command, articles }) => {
         if (command === 'newHeadlines') {
-          console.log(articles)
+          setNewsArticles(articles)
         }
       },
     })
